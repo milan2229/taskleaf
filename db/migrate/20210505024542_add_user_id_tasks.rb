fef0c44fe01d4880ffe40d59b42d8c371ac12283
milan2229 <milan_1899.22@icloud.com>
2021-05-05 15:33:13 +0900

    chapter4 finish

---
+++ db/migrate/20210505024542_add_user_id_tasks.rb
@@ -0,0 +1,10 @@
+class AddUserIdTasks < ActiveRecord::Migration[5.2]
+  def up
+    execute 'DELETE FROM tasks;'
+    add_reference :tasks, :user, null: false, index: true
+  end
+
+  def down
+    remove_reference :takss, :user, index: true
+  end
+end
